e928fdef11e171b97c5a35d74248cce201b3726f
kosmical <david.mario.pieri@gmail.com>
2021-04-25 18:06:42 +0200

    built post controllers

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -1,18 +1,31 @@
 const pool = require('../middleware/db');
 
-exports.getAllPosts = () => {
-
+exports.getAllPosts = async (req, res) => {
+  try {
+    const allPosts = await pool.query(
+      'SELECT * FROM posts'
+    );
+    res.status(200).json(allPosts.rows);
+  } catch (error) {
+    res.status(404).json(error);
+  }
 };
-exports.getTopicPosts = () => {
 
-};
-exports.getOnePost = () => {
-
+exports.getOnePost = async (req, res) => {
+  try {
+    const {id} = req.params;
+    const selectedPost = await pool.query(
+      'SELECT * FROM posts WHERE post_id = $1', [id]
+    );
+    res.status(200).json(selectedPost.rows[0]);
+  } catch (error) {
+    res.status(404).json(error);
+  }
 };
 
 exports.createPost = async (req, res) => {
   try {
-    // Get form data form request body
+    // Get form data from request body
     const {user_id, topic_id, title, body} = req.body;
     // Save form data to database table 'posts'
     const newPost = await pool.query(
@@ -20,20 +33,55 @@ exports.createPost = async (req, res) => {
       VALUES ($1, $2, $3, $4, $5) RETURNING *',
       [user_id, topic_id, title, body, new Date()]
     );
-    res.status(200).json(newPost);
+    res.status(200).json(newPost.rows[0]);
   } catch (error) {
     res.status(500).json(error);
   }
 };
 
-exports.setVoteStatus = () => {
-
+exports.updatePost = async (req, res) => {
+  try {
+    const {topic_id, title, body} = req.body;
+    const {id} = req.params;
+    const updatedPost = await pool.query(
+      'UPDATE posts SET topic_id = $1, post_title = $2, post_body = $3 \
+      WHERE post_id = $4 RETURNING *',
+      [topic_id, title, body, id]
+    );
+    res.status(200).json(updatedPost.rows[0]);
+  } catch (error) {
+    res.status(404).json(error);
+  }
 };
-exports.updatePost = () => {
 
+exports.setVoteStatus = async (req, res) => {
+  try {
+    const {id} = req.params;
+    const {user_id, vote} = req.body;
+    const post = await pool.query(
+      'SELECT * FROM posts WHERE post_id = $1', [id]
+    );
+    if (sauce.usersLiked === null) {
+      sauce.usersLiked = [];
+    }
+    if (sauce.usersDisliked === null) {
+        sauce.usersDisliked = [];
+    }
+  } catch (error) {
+    res.status(404).json(error);
+  }
 };
-exports.deletePost = () => {
 
+exports.deletePost = async (req, res) => {
+  try {
+    const {id} = req.params;
+    await pool.query(
+      'DELETE FROM posts WHERE post_id = $1', [id]
+    );
+    res.status(200).json({message: 'Post successfully deleted!'});
+  } catch (error) {
+    res.status(404).json(error);
+  }
 };
 
 
