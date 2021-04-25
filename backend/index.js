bdda5b3225780aa047adaf116280ff33c7dc17fb
kosmical <david.mario.pieri@gmail.com>
2021-04-25 14:01:55 +0200

    set headers & routes

--- backend/index.js
+++ backend/index.js
@@ -1,47 +1,28 @@
 const express = require('express');
-const pool = require('./db');
+
+const postRoutes = require('./routes/post');
+const userRoutes = require('./routes/user');
+const commentRoutes = require('./routes/comment');
+
 const app = express();
-
-
-app.use(express.json());
-
-
-// ROUTES //
-
-// Sign up
-
-// Sign in
-
-// Get all posts
-
-// Get one post
-
-// Create post
-
-// Update post
-
-// Delete post
-
-// Upvote or Downvote post
-
-
-// Get all comments
-
-// Create comment
-
-// Update comment
-
-// Delete comment
-
-// Upvote or downvote comment
 
 
 // Get all posts from topic
 
+// Set which headers and methods are allowed
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
+app.use(express.json());
 
+app.use('/api/posts', postRoutes);
+app.use('/api/auth', userRoutes);
+app.use('/api/comments', commentRoutes);
 
-
-app.listen(3000, () => {
-  console.log('server started on port 3000')
+app.listen(5000, () => {
+  console.log('server started on port 5000')
 })
