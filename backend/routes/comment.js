4d817fdf5b61c12c1f1282f5fd56775c549d41ee
kosmical <david.mario.pieri@gmail.com>
2021-04-25 14:04:13 +0200

    created routes

---
+++ backend/routes/comment.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const commentCtrl = require('../controllers/comment');
+
+router.get('/', commentCtrl.getAllComments);
+router.post('/', commentCtrl.createComment);
+router.post('/:id/vote', commentCtrl.setVoteStatus);
+router.put('/:id', commentCtrl.updateComment);
+router.delete('/:id', commentCtrl.deleteComment);
+
+module.exports = router;
---
+++ backend/routes/post.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const postCtrl = require('../controllers/post');
+
+router.get('/', postCtrl.getAllPosts);
+router.get('/', postCtrl.getTopicPosts);
+router.get('/:id', postCtrl.getOnePost);
+router.post('/', postCtrl.createPost);
+router.post('/:id/vote', postCtrl.setVoteStatus);
+router.put('/:id', postCtrl.updatePost);
+router.delete('/:id', postCtrl.deletePost);
+
+module.exports = router;
