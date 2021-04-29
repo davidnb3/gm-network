e6d39b1e0344d3f98434bafd41081e2b8a665105
kosmical <david.mario.pieri@gmail.com>
2021-04-29 13:38:16 +0200

    set up routes and data fetching

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,44 +1,89 @@
 import { Container } from "@chakra-ui/react"
-import {useState} from 'react';
-import Post from './components/post';
+import { useState, useEffect } from 'react';
+import Post from './components/posts';
 import CreatePost from './components/createPost';
 import Topics from './components/topics';
 import Navbar from './components/navbar';
+import SinglePost from "./components/singlePost";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
 
 export default function App() {
-  const [posts, setPosts] = useState([
-    {id: 1, title: 'First Post', body: 'This is the first post from the GM-Network'},
-    {id: 2, title: 'Second Post', body: 'This is the second post from the GM-Network and with a lot more text dhfaishffgksrugfhresughresugzehlrsighesrufhlreihsfuhelifhisehufkasnrdhjskghsdrlgshrlugihesigruhgseklurhesligesuhgheieurghsihmmmmmmmmmmmmmmmmmiuehfsaihudhuhuhuhwmwmwmwmwmwmwmwmwmmwmwmwmwmwwmmwmwwmmwmwmwm'},
-    {id: 3, title: 'Third Post', body: 'This is the third post from the GM-Network'},
-  ])
+  const [topics, setTopics] = useState([]);
+  const [posts, setPosts] = useState([]);
 
-  const [topics, setTopics] = useState([
-    {id: 1, title: 'Announcements'},
-    {id: 2, title: 'Technical Questions'},
-    {id: 3, title: 'General Questions'},
-    {id: 4, title: 'Memes'},
-    {id: 5, title: 'Music'},
-  ])
+  async function getTopics() {
+    try {
+       const response = await fetch('http://localhost:5000/api/topics');
+       const data = await response.json();
+       if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      capitalizeFirstLetter(data)
+      setTopics(data);
+     } catch (error) {
+       console.log(error)
+     }
+  };
+
+  function capitalizeFirstLetter(data) {
+    for (let i in data) {
+      return data[i].topic_name.charAt(0).toUpperCase() + data[i].topic_name.slice(1);
+    }
+  }
+
+  async function getPosts() {
+    try {
+      const response = await fetch('http://localhost:5000/api/posts');
+      const data = await response.json();
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      setPosts(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+    getTopics();
+  }, [] );
+
 
   return (
-    <div style={{
-        backgroundColor:'#dbdae8',
-        height: '100vh'
-        }}
-      >
-      <Navbar />
-      <Container maxW="container.lg" d='flex'>
+    <Router>
+      <div style={{
+          backgroundColor:'#dbdae8',
+          height: '100vh'
+          }}
+        >
+        <Navbar />
+          <Switch>
+            <Route path='/' exact>
+              <Container maxW="container.lg" d='flex' marginTop={6}>
 
-        <Container maxW="container.md" p={0}>
-            <CreatePost setPosts={setPosts} topics={topics}/>
-            <Post posts={posts}/>
-        </Container>
+                <Container maxW="container.md" p={0} m='0 20px 0 20px'>
+                  <CreatePost topics={topics}/>
+                  <Post posts={posts} setPosts={setPosts} />
+                </Container>
 
-        <Container maxW="container.sm" p={0}>
-          <Topics topics={topics}/>
-        </Container>
+                <Container flex='1' maxW="container.sm" p={0}>
+                  <Topics topics={topics} setTopics={setTopics}/>
+                </Container>
 
-      </Container>
-    </div>
+              </Container>
+            </Route>
+
+            <Route path={'/:id'} component={SinglePost} />
+
+          </Switch>
+
+      </div>
+    </Router>
   );
-}
+};
