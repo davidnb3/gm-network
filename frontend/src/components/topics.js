5f43569a5bcb8ba8e09559cb4b17e6db9a0a5c70
kosmical <david.mario.pieri@gmail.com>
2021-04-29 13:43:42 +0200

    added fragment + corrected props

--- frontend/src/components/topics.js
+++ frontend/src/components/topics.js
@@ -2,14 +2,16 @@ import { Box, ListItem, UnorderedList } from "@chakra-ui/react"
 
 export default function Topics({topics}) {
   return(
-    <UnorderedList marginTop={6} styleType='none'>
-      {topics.map((topic) => (
-        <ListItem key={topic.id}>
-          <Box bg="white" w="100%" p={4} borderWidth="1px" borderRadius='5px'>
-          {topic.title}
-          </Box>
-        </ListItem>
-      ))}
-    </UnorderedList>
+    <>
+      <UnorderedList styleType='none' marginLeft='inherit'>
+        {topics.map((topic) => (
+          <ListItem key={topic.topic_id} >
+            <Box bg="white" w="100%" p={4} borderWidth="1px" borderRadius='5px'>
+            {topic.topic_name}
+            </Box>
+          </ListItem>
+        ))}
+      </UnorderedList>
+    </>
   )
 }
