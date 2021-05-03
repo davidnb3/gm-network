fd3f895739fc4bd48191d9f2cecfa5aeb8c06442
kosmical <david.mario.pieri@gmail.com>
2021-05-03 16:40:13 +0200

    posts styling

--- frontend/src/components/posts.js
+++ frontend/src/components/posts.js
@@ -1,7 +1,8 @@
-import { Box, VStack, Heading } from "@chakra-ui/react"
+import { VStack, Heading, HStack, Flex, Container, Text } from "@chakra-ui/react"
+import { GoArrowUp, GoArrowDown } from "react-icons/go";
 import { Link } from "react-router-dom";
 
-export default function Post({posts}) {
+export default function Posts({posts}) {
 
   return(
     <>
@@ -9,21 +10,45 @@ export default function Post({posts}) {
         <VStack spacing={6}  marginBottom={6} >
           {posts.map((post) => (
             <Link
-              to={`/${post.post_id}`}
+              to={`/post/${post.post_id}`}
               key={post.post_id}
-              style={{width: '100%'}}>
-            <Box 
-              bg="white"
-              p={4}
-              borderColor='#CBD5E0'
-              borderWidth='1px'
-              borderRadius='5px'
+              style={{width: '100%'}}
             >
-              <p>Topic ID:{post.topic_id}</p>
-              <p>User ID: {post.user_id}</p>
-              <Heading size='sm'> {post.post_title} </Heading>
-              <p> {post.post_body} </p>
-            </Box>
+              <Flex
+                bg="white"
+                p={0}
+                borderColor='#CBD5E0'
+                borderWidth='1px'
+                borderRadius='5px'
+                transition='0.4s'
+                _hover={{
+                  boxShadow:"xl"
+                }}
+              >
+                <Container
+                  maxW='40px'
+                  p={4}
+                  m={0}
+                  bg='#FAF5FF'
+                  borderRadius='5px 0 0 5px'
+                  d='flex'
+                  flexDirection='column'
+                  alignItems='center'
+                  justifyContent='center'
+                >
+                  <GoArrowUp size={20} style={{color : 'grey'}}/>
+                    <Text style={{fontSize: '0.9rem'}}>0</Text>
+                  <GoArrowDown size={20} style={{color : 'grey'}}/>
+                </Container>
+                <Container p='8px 24px 24px 24px' m={0} w='100%' lineHeight='1.8rem'>
+                  <HStack fontSize='xs' marginBottom='4px'>
+                    <Text>Topic ID:{post.topic_id}</Text>
+                    <Text style={{color : 'grey'}}>Posted by: User ID: {post.user_id}</Text>
+                  </HStack>
+                  <Heading size='md' fontWeight='500'> {post.post_title} </Heading>
+                  <Text> {post.post_body} </Text>
+                </Container>
+              </Flex>
             </Link>
           ))}
         </VStack>
