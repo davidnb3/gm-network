32d471fd63adcc4681d12de44c250afbc9083ce1
kosmical <david.mario.pieri@gmail.com>
2021-05-17 16:57:01 +0200

    created separate login + signup page

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,94 +1,99 @@
 import {
   Button,
+  Container,
   FormControl,
   FormLabel,
   Input,
   InputGroup,
   InputRightElement,
-  Modal,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  ModalOverlay,
   VStack,
-  useDisclosure,
+  Center,
+  Link
 } from "@chakra-ui/react";
 import { useState } from 'react';
+import { Link as RouterLink } from "react-router-dom";
 
-export default function LogIn() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+export default function LogIn({setToken}) {
   const  [ username, setUsername ] = useState('');
   const [ password, setPassword ] = useState('');
   const [ showPassword, setShowPassword ] = useState(false);
   const isInvalid = password === '' || username === '';
   
-  function handleSignIn(event) {
+  async function handleSignIn(event) {
     event.preventDefault();
-    console.log('password', password);
-    console.log('username', username);
+    try {
+      const user = {
+        username,
+        password
+      }
+      const response = await fetch(('http://localhost:5000/api/auth/login'), {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(user)
+      })
+      const data = await response.json();
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      setToken(data)
+      window.location = '/';
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return(
     <>
-      <Button onClick={onOpen} colorScheme="blue" w={92}>
-        Log In
-      </Button>
-      <Modal onClose={onClose} isOpen={isOpen}>
-        <ModalOverlay>
-          <ModalContent>
-            <ModalHeader>Log In</ModalHeader>
-            <ModalCloseButton />
-            <form method='POST' onSubmit={handleSignIn}>
-              <ModalBody>
-                <VStack spacing={6}>
-                  <FormControl isRequired>
-                    <FormLabel htmlFor='username'>User Name</FormLabel>
-                    <Input
-                      type='text'
-                      id='username'
-                      value={username}
-                      onChange={({ target }) => setUsername(target.value)}
-                    />
-                  </FormControl>
-                  <FormControl isRequired>
-                    <FormLabel htmlFor='password'>Password</FormLabel>
-                    <InputGroup>
-                      <Input
-                        type={showPassword ? 'text' : 'password'}
-                        id='password'
-                        autoComplete='off'
-                        value={password}
-                        onChange={({ target }) => setPassword(target.value)}
-                      />
-                      <InputRightElement width="4.5rem">
-                        <Button
-                          h="1.75rem"
-                          size="sm"
-                          onClick={() => setShowPassword(!showPassword)}
-                        >
-                          {showPassword ? 'Hide' : 'Show'}
-                        </Button>
-                      </InputRightElement>
-                    </InputGroup>
-                  </FormControl>
-                </VStack>
-              </ModalBody>
-              <ModalFooter>
-                  <Button
-                    type='submit'
-                    disabled={isInvalid}
-                    colorScheme="pink"
-                    variant="solid"
-                  > 
-                      Submit
-                  </Button>
-              </ModalFooter>
-            </form>
-          </ModalContent>
-        </ModalOverlay>
-      </Modal>
+      <Center h='100vh'>
+        <Container maxW='md' bg="white" p={8} borderRadius='5px'>
+          <form method='POST' onSubmit={handleSignIn}>
+            <VStack spacing={6}>
+              <FormControl isRequired>
+                <FormLabel htmlFor='username'>User Name</FormLabel>
+                <Input
+                  type='text'
+                  id='username'
+                  value={username}
+                  onChange={({ target }) => setUsername(target.value)}
+                />
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel htmlFor='password'>Password</FormLabel>
+                <InputGroup>
+                  <Input
+                    type={showPassword ? 'text' : 'password'}
+                    id='password'
+                    autoComplete='off'
+                    value={password}
+                    onChange={({ target }) => setPassword(target.value)}
+                  />
+                  <InputRightElement width="4.5rem">
+                    <Button
+                      h="1.75rem"
+                      size="sm"
+                      onClick={() => setShowPassword(!showPassword)}
+                    >
+                      {showPassword ? 'Hide' : 'Show'}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+              </FormControl>
+              <Button
+                type='submit'
+                disabled={isInvalid}
+                colorScheme="purple"
+                variant="solid"
+                w='100%'
+              > 
+                  Login
+              </Button>
+              <Link as={RouterLink} to='/signup' color='purple.400'>
+                Don't have an account?
+              </Link>
+            </VStack>
+          </form>
+        </Container>
+      </Center>
     </>
   );
 };
--- frontend/src/components/signup.js
+++ frontend/src/components/signup.js
@@ -1,109 +1,113 @@
 import {
   Button,
+  Container,
   FormControl,
   FormLabel,
   FormHelperText,
   Input,
   InputGroup,
   InputRightElement,
-  Modal,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  ModalOverlay,
   VStack,
-  useDisclosure,
+  Center,
+  Link
 } from "@chakra-ui/react";
 import { useState } from 'react';
+import { Link as RouterLink } from "react-router-dom";
 
 export default function Signup() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  
   const  [ email, setEmail ] = useState('');
   const [ password, setPassword ] = useState('');
   const [ username, setUsername ] = useState('');
   const [ showPassword, setShowPassword ] = useState(false);
   const isInvalid = password === '' || email === '';
   
-  function handleSignUp(event) {
+  async function handleSignUp(event) {
     event.preventDefault();
-    console.log('password', password);
-    console.log('email', email);
+    try {
+      const user = {
+        username,
+        email,
+        password
+      }
+      const response = await fetch(('http://localhost:5000/api/auth/signup'), {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(user)
+      })
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return(
     <>
-      <Button onClick={onOpen} colorScheme="blue" w={92}>
-        Sign Up
-      </Button>
-      <Modal onClose={onClose} isOpen={isOpen}>
-        <ModalOverlay>
-          <ModalContent>
-            <ModalHeader>Sign Up</ModalHeader>
-            <ModalCloseButton />
-            <form method='POST' onSubmit={handleSignUp}>
-              <ModalBody>
-                <VStack spacing={6}>
-                <FormControl isRequired>
-                    <FormLabel htmlFor='username'>User Name</FormLabel>
-                    <Input
-                      type='text'
-                      id='username'
-                      value={username}
-                      onChange={({ target }) => setUsername(target.value)}
-                    />
-                  </FormControl>
-                  <FormControl isRequired>
-                    <FormLabel htmlFor='email'>Email Address</FormLabel>
-                    <Input
-                      type='email'
-                      id='email'
-                      aria-describedby='email-helper-text'
-                      value={email}
-                      onChange={({ target }) => setEmail(target.value)}
-                    />
-                    <FormHelperText id='email-helper-text'>
-                      Please use your internal email address.
-                    </FormHelperText>
-                  </FormControl>
-                  <FormControl isRequired>
-                    <FormLabel htmlFor='password'>Password</FormLabel>
-                    <InputGroup>
-                      <Input
-                        type={showPassword ? 'text' : 'password'}
-                        id='password'
-                        autoComplete='off'
-                        value={password}
-                        onChange={({ target }) => setPassword(target.value)}
-                      />
-                      <InputRightElement width="4.5rem">
-                        <Button
-                          h="1.75rem"
-                          size="sm"
-                          onClick={() => setShowPassword(!showPassword)}
-                        >
-                          {showPassword ? 'Hide' : 'Show'}
-                        </Button>
-                      </InputRightElement>
-                    </InputGroup>
-                  </FormControl>
-                </VStack>
-              </ModalBody>
-              <ModalFooter>
-                  <Button
-                    type='submit'
-                    disabled={isInvalid}
-                    colorScheme="pink"
-                    variant="solid"
-                  > 
-                      Submit
-                  </Button>
-              </ModalFooter>
-            </form>
-          </ModalContent>
-        </ModalOverlay>
-      </Modal>
+      <Center h='100vh'>
+        <Container maxW='md' bg="white" p={8} borderRadius='5px'>
+          <form method='POST' onSubmit={handleSignUp}>
+            <VStack spacing={6}>
+              <FormControl isRequired>
+                <FormLabel htmlFor='username'>User Name</FormLabel>
+                <Input
+                  type='text'
+                  id='username'
+                  value={username}
+                  onChange={({ target }) => setUsername(target.value)}
+                />
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel htmlFor='email'>Email Address</FormLabel>
+                <Input
+                  type='email'
+                  id='email'
+                  aria-describedby='email-helper-text'
+                  value={email}
+                  onChange={({ target }) => setEmail(target.value)}
+                />
+                <FormHelperText id='email-helper-text'>
+                  Please use your internal email address.
+                </FormHelperText>
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel htmlFor='password'>Password</FormLabel>
+                <InputGroup>
+                  <Input
+                    type={showPassword ? 'text' : 'password'}
+                    id='password'
+                    autoComplete='off'
+                    value={password}
+                    onChange={({ target }) => setPassword(target.value)}
+                  />
+                  <InputRightElement width="4.5rem">
+                    <Button
+                      h="1.75rem"
+                      size="sm"
+                      onClick={() => setShowPassword(!showPassword)}
+                    >
+                      {showPassword ? 'Hide' : 'Show'}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+              </FormControl>
+              <Button
+                type='submit'
+                disabled={isInvalid}
+                colorScheme="purple"
+                variant="solid"
+                w='100%'
+                > 
+                  Submit
+              </Button>
+              <Link as={RouterLink} to='/' color='purple.400'>
+                Already have an account?
+              </Link>
+            </VStack>
+          </form>
+        </Container>
+      </Center>
     </>
   );
 };
