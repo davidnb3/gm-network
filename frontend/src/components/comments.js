93823fd8c0aca1b9534427c3cb3c553e2b5dc622
kosmical <david.mario.pieri@gmail.com>
2021-05-17 16:55:53 +0200

    moved comments fetch to parent

--- frontend/src/components/comments.js
+++ frontend/src/components/comments.js
@@ -1,51 +1,22 @@
-import { Box, VStack, Text } from "@chakra-ui/react"
-import { useState, useEffect } from 'react';
-import { useParams } from "react-router-dom";
+import { Box, Text } from "@chakra-ui/react"
 
-
-export default function Comments() {
-  const [ comments, setComments ] = useState([]);
-  const {id} = useParams();
-
-  async function getComments() {
-    try {
-      const response = await fetch(`http://localhost:5000/api/comments/${id}`);
-      const data = await response.json();
-      if (!response.ok) {
-        throw new Error(response.statusText);
-      }
-      setComments(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    getComments();
-  }, [] );
+export default function Comments({comment}) {
 
   return (
     <>
-    <VStack spacing={6}>
-          {comments.map((comment) => (
-            
-            <Box
-              key={comment.comment_id}
-              style={{width: '100%'}} 
-              bg="white"
-              marginTop={12}
-              p={4}
-              borderColor='#CBD5E0'
-              borderWidth='1px'
-              borderRadius='5px'
-            >
-              
-              <Text color='grey' fontSize='xs'>Posted by: User ID: {comment.user_id}</Text>
-              <Text> {comment.comment_body} </Text>
-            </Box>
-            
-          ))}
-        </VStack>
+      <Box
+        key={comment.comment_id}
+        style={{width: '100%'}} 
+        bg="white"
+        marginTop={12}
+        p={4}
+        borderColor='#CBD5E0'
+        borderWidth='1px'
+        borderRadius='5px'
+      >
+        <Text color='grey' fontSize='xs' mb={2}>Posted by: {comment.user_name}</Text>
+        <Text> {comment.comment_body} </Text>
+      </Box>
     </>
   )
 }
