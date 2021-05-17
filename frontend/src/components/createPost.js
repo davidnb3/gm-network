8ba2fc37f8d6b93341b2238268e21ec14b159fca
kosmical <david.mario.pieri@gmail.com>
2021-05-17 16:59:08 +0200

    refactor to arrow function + various fixes

--- frontend/src/components/createPost.js
+++ frontend/src/components/createPost.js
@@ -6,27 +6,28 @@ import {Box,
   Collapse,
   Textarea,
   Button,
-  HStack} from "@chakra-ui/react";
+  HStack,
+} from "@chakra-ui/react";
 
-export default function CreatePost({topics}) {
+export default function CreatePost({topics, authToken, userId}) {
   const { isOpen, onOpen } = useDisclosure();
   const [title, setTitle] = useState('');
   const [text, setText] = useState('');
   const [selectTopic, setSelectTopic] = useState();
   const invalid = title === '' || selectTopic === undefined;
 
-  async function handleAddPost(event) {
+  const handleAddPost = async (event) => {
     event.preventDefault();
     try {
       const post = {
-        user_id: 1,
+        user_id: userId,
         topic_id: selectTopic,
         title: title,
         body: text
       };
       const response = await fetch(('http://localhost:5000/api/posts'), {
         method: 'POST',
-        headers: {'Content-Type': 'application/json'},
+        headers: {'Content-Type': 'application/json', 'Authorization': `Bearer ${authToken}`}, 
         body: JSON.stringify(post)
       });
       if (!response.ok) {
@@ -55,7 +56,7 @@ export default function CreatePost({topics}) {
             boxShadow:"xl"
           }}
         >
-          <form method='POST' onSubmit={handleAddPost}>
+          <form method='POST' onSubmit={() => handleAddPost()}>
             <FormControl>
               <Input
                 isRequired
@@ -75,27 +76,35 @@ export default function CreatePost({topics}) {
                   value={text}
                   placeholder='Text (optional)'
                   marginTop={2}
+                  h='130px'
                   focusBorderColor="#E9D8FD"
                   borderWidth='1px'
                   borderColor='gray.200'
                   onChange={({target}) => setText(target.value)}
                 />
-                <HStack spacing={4}>
+                <HStack spacing={4} marginBottom={2} marginTop={1}>
                   {topics.map((topic) => (
                     <Button
                       key={topic.topic_id}
                       id={topic.topic_id}
                       value={topic.topic_id}
                       type='button'
-                      colorScheme="teal"
+                      colorScheme="purple"
                       size="xs"
+                      variant='outline'
                       onClick={({target}) => setSelectTopic(target.value)}
                     >
                       {topic.topic_name}
                     </Button>
                   ))};
                 </HStack>
-                <Button type='submit' disabled={invalid} w={92}>Post</Button>
+                <Button
+                  colorScheme='purple'
+                  type='submit'
+                  disabled={invalid}
+                  w={92}
+                >
+                  Post</Button>
               </Collapse>
             </FormControl>
           </form>
