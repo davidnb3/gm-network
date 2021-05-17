220dc65fa02246e5548d68bd807c9a972538c7f4
kosmical <david.mario.pieri@gmail.com>
2021-05-17 16:53:47 +0200

    set up auth + protected routes

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,24 +1,31 @@
-import { Container } from "@chakra-ui/react"
+import { Container, VStack } from "@chakra-ui/react"
 import { useState, useEffect } from 'react';
 import Posts from './components/posts';
 import CreatePost from './components/createPost';
 import Topics from './components/topics';
 import Navbar from './components/navbar';
 import SinglePost from "./components/singlePost";
+import Signup from './components/signup';
+import LogIn from './components/login';
+import useToken from './hooks/useToken';
 import {
   BrowserRouter as Router,
   Switch,
-  Route
+  Route,
 } from "react-router-dom";
-
 
 export default function App() {
   const [topics, setTopics] = useState([]);
   const [posts, setPosts] = useState([]);
-
-  async function getTopics() {
+  const {token, setToken} = useToken();
+  const authToken = token?.authentication;
+  const userId = token?.userId;
+  
+  const getTopics = async () => {
     try {
-       const response = await fetch('http://localhost:5000/api/topics');
+       const response = await fetch(('http://localhost:5000/api/topics'), {
+         headers: {'Authorization': `Bearer ${authToken}`}
+       });
        const data = await response.json();
        if (!response.ok) {
         throw new Error(response.statusText);
@@ -29,9 +36,11 @@ export default function App() {
      }
   };
 
-  async function getPosts() {
+  const getPosts = async () => {
     try {
-      const response = await fetch('http://localhost:5000/api/posts');
+      const response = await fetch(('http://localhost:5000/api/posts'), {
+        headers: {'Authorization': `Bearer ${authToken}`}
+      });
       const data = await response.json();
       if (!response.ok) {
         throw new Error(response.statusText);
@@ -42,45 +51,83 @@ export default function App() {
     }
   };
 
-  
-
   useEffect(() => {
     getPosts();
     getTopics();
   }, [] );
 
+  if (!token) {
+    return (
+      <div style={{
+        backgroundColor:'#dbdae8',
+        minHeight: '100vh',
+        height: '100%'
+        }}
+      >
+        <Router>
+          <Switch>
+            <Route exact path='/'>
+              <LogIn setToken={setToken}/>
+            </Route>
+            <Route exact path='/signup'>
+              <Signup />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    )
+  }
 
   return (
     <Router>
       <div style={{
-          backgroundColor:'#dbdae8',
-          minHeight: '100vh',
-          height: '100%'
-          }}
-        >
+        backgroundColor:'#dbdae8',
+        minHeight: '100vh',
+        height: '100%',
+        paddingBottom: '20px'
+        }}
+      >
         <Navbar />
-          <Switch>
-            <Route path='/' exact>
-              <Container maxW="container.lg" d='flex' marginTop={6}>
-
-                <Container maxW="container.md" p={0} m='0 20px 0 20px'>
-                  <CreatePost topics={topics}/>
-                  <Posts posts={posts} setPosts={setPosts} />
-                </Container>
-
-                <Container flex='1' maxW="container.sm" p={0}>
-                  <Topics topics={topics} setPosts={setPosts}/>
-                </Container>
-
+        <Switch>
+          <Route exact path='/'>
+            <Container maxW="container.lg" d='flex' marginTop={6}>
+              <Container maxW="container.md" p={0} m='0 20px 0 20px'>
+                <CreatePost
+                  topics={topics}
+                  authToken={authToken}
+                  userId={userId}
+                />
+                <VStack spacing={6}>
+                  {posts.map((post) => (
+                    <Posts
+                      post={post}
+                      authToken={authToken}
+                      setPosts={setPosts}
+                      userId={userId}
+                      key={post.post_id}
+                    />
+                  ))}
+                </VStack>
               </Container>
-            </Route>
+              <Container flex='1' maxW="container.sm" p={0}>
+                <Topics
+                  topics={topics}
+                  setPosts={setPosts}
+                  getPosts={getPosts}
+                  authToken={authToken}
+                />
+              </Container>
+            </Container>
+          </Route>
 
-            <Route path={'/post/:id'} render={() => (
-              <SinglePost />
-            )}/>
-
-          </Switch>
-
+          <Route exact path={'/post/:id'} render={() => (
+            <SinglePost
+              authToken={authToken}
+              userId={userId}
+              topics={topics}
+            />
+          )}/>
+        </Switch>
       </div>
     </Router>
   );
