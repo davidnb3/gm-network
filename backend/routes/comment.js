aa433968a4536586997a89547f13acca489925d1
kosmical <david.mario.pieri@gmail.com>
2021-05-17 16:48:42 +0200

    removed vote function

--- backend/routes/comment.js
+++ backend/routes/comment.js
@@ -6,7 +6,6 @@ const auth = require('../middleware/auth');
 
 router.get('/:id', auth, commentCtrl.getAllComments);
 router.post('/', auth, commentCtrl.createComment);
-router.post('/:id/vote', auth, commentCtrl.setVoteStatus);
 router.put('/:id', auth, commentCtrl.updateComment);
 router.delete('/:id', auth, commentCtrl.deleteComment);
 
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -7,7 +7,6 @@ const auth = require('../middleware/auth');
 router.get('/', auth, postCtrl.getAllPosts);
 router.get('/:id', auth, postCtrl.getOnePost);
 router.post('/', auth, postCtrl.createPost);
-router.post('/:id/vote', auth, postCtrl.setVoteStatus);
 router.put('/:id', auth, postCtrl.updatePost);
 router.delete('/:id', auth, postCtrl.deletePost);
 
