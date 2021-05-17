3205a384be73d9bc74b55761da50f3d61a4404a3
kosmical <david.mario.pieri@gmail.com>
2021-05-17 16:58:46 +0200

    removed signup/login button from navbar

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,16 +1,12 @@
-import { Box, Container, Flex, HStack } from "@chakra-ui/react";
-import Signup from './signup';
-import LogIn from './login';
+import { Box, Container, Flex } from "@chakra-ui/react";
+import Logout from './logout';
 
 export default function Navbar() {
   return (
     <Box position="sticky" top={0} p={2} bg="gray.100" zIndex={1}>
       <Container maxW="container.lg">
         <Flex justifyContent="flex-end" w="100%" position="sticky" top={0}>
-          <HStack spacing={2}>
-            <LogIn />
-            <Signup />
-          </HStack>
+          <Logout />
         </Flex>
       </Container>
     </Box>
