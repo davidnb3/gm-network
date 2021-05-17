1a6d8636301c7b982faf43656f5c48c240ac43f0
kosmical <david.mario.pieri@gmail.com>
2021-05-17 16:56:23 +0200

    added a delete post alert

---
+++ frontend/src/components/deleteAlert.js
@@ -0,0 +1,41 @@
+import {
+  Button,
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
+} from "@chakra-ui/react"
+import { useRef } from 'react';
+
+export default function DeleteAlert({onClose, isOpen, handleDeletePost}) {
+  const cancelRef = useRef()
+
+  return (
+    <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              Delete Post
+            </AlertDialogHeader>
+            <AlertDialogBody>
+              Are you sure? You can't undo this action afterwards.
+            </AlertDialogBody>
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose}>
+                Cancel
+              </Button>
+              <Button colorScheme="red" onClick={onClose, () => handleDeletePost()} ml={3}>
+                Delete
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+  )
+}
