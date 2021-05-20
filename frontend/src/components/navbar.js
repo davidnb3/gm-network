fc0792a03be0da1a6485b1156231734b81ef9d7b
kosmical <david.mario.pieri@gmail.com>
2021-05-20 15:10:47 +0200

    refactored navbar

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,12 +1,34 @@
-import { Box, Container, Flex } from "@chakra-ui/react";
-import Logout from './logout';
+import {
+  Box,
+  Container,
+  Flex,
+  Button,
+  HStack 
+} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
+
+  const handleLogout = () => {
+    sessionStorage.removeItem('token');
+    window.location = '/';
+    console.log('ok')
+  }
+
   return (
     <Box position="sticky" top={0} p={2} bg="gray.100" zIndex={1}>
       <Container maxW="container.lg">
         <Flex justifyContent="flex-end" w="100%" position="sticky" top={0}>
-          <Logout />
+          <HStack spacing={4}>
+            <Link to='/account'>
+              <Button minWidth='95px' colorScheme="purple">
+                Account
+              </Button>
+            </Link>
+            <Button minWidth='95px' colorScheme="purple" onClick={() => handleLogout()}>
+              Logout
+            </Button>
+          </HStack>
         </Flex>
       </Container>
     </Box>
