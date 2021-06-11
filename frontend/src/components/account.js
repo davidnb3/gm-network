209a5a19fe963c163ebdcf3876b6eae01c801dc4
kosmical <david.mario.pieri@gmail.com>
2021-06-12 00:02:32 +0200

    removed container and added avatar

--- frontend/src/components/account.js
+++ frontend/src/components/account.js
@@ -3,12 +3,14 @@ import {
   Heading,
   Text,
   Tooltip,
-  Button
+  Button,
+  Avatar
 } from "@chakra-ui/react";
 import { useState, useEffect } from 'react';
 import { ArrowBackIcon } from '@chakra-ui/icons';
 import postDataToApi from '../api/postDataToApi';
 import DeleteAlert from './deleteAlert';
+import Icon from '../assets/images/icon.svg';
 
 export default function Account({userId, authToken}) {
   const [userData, setUserData] = useState({});
@@ -35,53 +37,51 @@ export default function Account({userId, authToken}) {
   }, [userId, authToken] );
 
   return (
-    <div style={{
-      backgroundColor:'#dbdae8',
-      minHeight: '100vh',
-      height: '100%',
-      paddingBottom: '20px'
-      }}
+    <Container
+      maxW="container.md"
+      m={0}
+      bg="white"
+      w="100%"
+      p={6}
+      borderColor='#CBD5E0'
+      borderWidth='1px'
+      borderRadius='5px'
     >
-      <Container
-        maxW="container.md"
-        marginTop={6}
-        bg="white"
-        w="100%"
-        p={6}
-        borderColor='#CBD5E0'
-        borderWidth='1px'
-        borderRadius='5px'
-      >
-        <Container m={0} p={0}>
-          <Tooltip label='Go back' fontSize='xs'>
-            <Button
-                p={0}
-                size='sm'
-                bg='transparent'
-                onClick={() => window.location = '/'}
-              >
-              <ArrowBackIcon boxSize='1.3em'/>
-            </Button>
-          </Tooltip>
-        </Container>
-        <Container m={0} p={0} textAlign='center' lineHeight='20px'>
-          <i className="far fa-user-circle fa-8x" style={{color: '#805AD5'}} ></i>
-          <Heading size='md'>{userData[0]?.user_name}</Heading>
-          <Text>{userData[0]?.user_email}</Text>
-          <Text>{userData[0]?.created_on}</Text>
-          <Button colorScheme="red" onClick={handleDeleteBtn}>
-            Delete Account
+      <Container m={0} p={0} maxW='100%'>
+        <Tooltip label='Go back' fontSize='xs'>
+          <Button
+              p={0}
+              size='sm'
+              bg='transparent'
+              onClick={() => window.location = '/'}
+            >
+            <ArrowBackIcon boxSize='1.3em'/>
           </Button>
-        </Container>
+        </Tooltip>
       </Container>
 
+      <Container
+        m={0}
+        p={0}
+        maxW='100%'
+        textAlign='center'
+        lineHeight='20px'
+      >
+        <Avatar name={userData[0]?.user_name} size='2xl' />
+        <Heading size='md'>{userData[0]?.user_name}</Heading>
+        <Text>{userData[0]?.user_email}</Text>
+        <Text>{userData[0]?.created_on}</Text>
+        <Button colorScheme="red" onClick={handleDeleteBtn}>
+          Delete Account
+        </Button>
+      </Container>
+      
       <DeleteAlert
         onClose={onClose}
         isOpen={isOpen}
         handleDelete={handleDelete}
         deleteBtn={deleteBtn}
       />
-
-    </div>
+    </Container>
   )
 }
