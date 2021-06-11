af37ad10baea10f896da2b09ad551caa4c4c3c7a
kosmical <david.mario.pieri@gmail.com>
2021-06-12 00:01:55 +0200

    added conditional

--- frontend/src/api/getDataFromApi.js
+++ frontend/src/api/getDataFromApi.js
@@ -1,16 +1,18 @@
 export default async function getDataFromApi(endpoint, id, authtoken, setState) {
-  try {
-    const response = await fetch((`http://localhost:5000/api/${endpoint}/${id}`), {
-      headers: {'Authorization': `Bearer ${authtoken}`}
-    });
-    const data = await response.json();
-    if (!response.ok) {
-      throw new Error(response.statusText);
+  if (authtoken) {
+    try {
+      const response = await fetch((`http://localhost:5000/api/${endpoint}/${id}`), {
+        headers: {'Authorization': `Bearer ${authtoken}`}
+      });
+      const data = await response.json();
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      if (setState) {
+        setState(data);
+      }
+    } catch (error) {
+      console.log(error);
     }
-    if (setState) {
-      setState(data);
-    }
-  } catch (error) {
-    console.log(error);
   }
 };
